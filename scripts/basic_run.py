a29c71ae2dc0a8d4d1a881544a11ad133b5f1fe2
ikeda042 <ikeda042main@gmail.com>
2024-06-18 15:50:11 +0900

    T

--- scripts/basic_run.py
+++ scripts/basic_run.py
@@ -1,24 +1,19 @@
 from opentrons import protocol_api, types
-from typing import Annotated, Literal
 from opentrons import simulate, execute
-import requests
 
 
-class OT2BasicProtocol:
-    def __init__(self) -> None:
-        self.metadata = {
-            "protocolName": "Basic Protocol",
-            "author": "ikeda042",
-            "description": "Basic Protocol for testing",
-        }
-        self.requirements = {"robotType": "OT-2", "apiLevel": "2.18"}
-        self.
-ROBOT_IP = "169.254.216.225"
-ROBOT_PORT = "31950"
+# class OT2BasicProtocol:
+#     def __init__(self) -> None:
+#         self.metadata = {
+#             "protocolName": "Basic Protocol",
+#             "author": "ikeda042",
+#             "description": "Basic Protocol for testing",
+#         }
+#         self.requirements = {"robotType": "OT-2", "apiLevel": "2.18"}
+#         self.
 
-
-PipetteType: str = Annotated[str, Literal["p20_multi_gen2", "p300_multi_gen2"]]
-MountType: str = Annotated[str, Literal["left", "right"]]
+# PipetteType: str = Annotated[str, Literal["p20_multi_gen2", "p300_multi_gen2"]]
+# MountType: str = Annotated[str, Literal["left", "right"]]
 
 metadata: dict[str, str] = {
     "protocolName": "OT-2 Basic Protocol",
@@ -32,15 +27,11 @@ requirements: dict[str, str] = {"robotType": "OT-2", "apiLevel": "2.11"}
 def run(protocol: protocol_api.ProtocolContext):
     plate = protocol.load_labware("corning_96_wellplate_360ul_flat", "2")
     tiprack = protocol.load_labware("opentrons_96_tiprack_300ul", "1")
-    right_pipette = protocol.load_instrument("")
-
-    # pipettes
-    left_pipette = protocol.load_instrument(
-        "p300_multi_gen2", mount="left", tip_racks=[tiprack]
+    right_pipette = protocol.load_instrument(
+        "p300_multi_gen2", "right", tip_racks=[tiprack]
     )
 
-    # commands
-    left_pipette.pick_up_tip()
-    left_pipette.aspirate(100, plate["A1"])
-    left_pipette.dispense(100, plate["B2"])
-    left_pipette.drop_tip()
+    right_pipette.pick_up_tip(tiprack.wells_by_name()["A1"])
+    right_pipette.aspirate(100, plate["A1"])
+    right_pipette.dispense(100, plate["B2"])
+    right_pipette.drop_tip(tiprack.wells_by_name()["B12"])
