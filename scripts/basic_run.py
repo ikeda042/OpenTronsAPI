c1f22972f40ce477e2533d88f0845228128c9c65
ikeda042 <ikeda042main@gmail.com>
2024-06-18 18:34:27 +0900

    T

--- scripts/basic_run.py
+++ scripts/basic_run.py
@@ -11,20 +11,16 @@ metadata = {
 def run(protocol: protocol_api.ProtocolContext):
     plate = protocol.load_labware("corning_96_wellplate_360ul_flat", "1")
     tray = protocol.load_labware("corning_96_wellplate_360ul_flat", "6")
+    tray2 = protocol.load_labware("corning_96_wellplate_360ul_flat", "4")
     plate_2 = protocol.load_labware("corning_96_wellplate_360ul_flat", "2")
     tiprack = protocol.load_labware("opentrons_96_tiprack_300ul", "7")
+
     right_pipette = protocol.load_instrument(
         "p300_multi_gen2", "right", tip_racks=[tiprack]
     )
-    right_pipette.pick_up_tip(tiprack.wells_by_name()["A1"])
-    for i in range(12):
+    right_pipette.pick_up_tip(tiprack.wells_by_name()["A3"])
+    for i in range(40):
         for i in range(12):
             right_pipette.aspirate(200, tray[f"A{i%12+1}"])
-            right_pipette.dispense(220, plate[f"A{i%12+1}"])
-        for i in range(12):
-            right_pipette.aspirate(100, plate[f"A{i%12+1}"])
-            right_pipette.dispense(110, plate_2[f"A{i%12+1}"])
-        for i in range(12):
-            right_pipette.aspirate(200, plate_2[f"A{i%12+1}"])
-            right_pipette.dispense(210, tray[f"A{i%12+1}"])
+            right_pipette.dispense(200, tray2[f"A{i%12+1}"])
     right_pipette.drop_tip(tiprack.wells_by_name()["A1"])
