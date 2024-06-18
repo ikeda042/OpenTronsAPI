4fa27d875083eec38fb4577592f53ee16dc0d174
ikeda042 <ikeda042main@gmail.com>
2024-06-18 23:53:13 +0900

    T

--- scripts/test.py
+++ scripts/test.py
@@ -13,31 +13,6 @@ metadata = {
 }
 
 
-def get_tiprack(
-    protocol: protocol_api.ProtocolContext,
-    tiprack_type: Literal["opentrons_96_tiprack_300ul", "opentrons_96_tiprack_20ul"],
-    slot: Literal["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"],
-) -> protocol_api.labware.Labware:
-    return protocol.load_labware(tiprack_type, slot)
-
-
-def load_plate(
-    protocol: protocol_api.ProtocolContext,
-    plate_type: Literal["corning_96_wellplate_360ul_flat"],
-    slot: Literal["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"],
-) -> protocol_api.labware.Labware:
-    return protocol.load_labware(plate_type, slot)
-
-
-def load_pipette(
-    protocol: protocol_api.ProtocolContext,
-    pipette_type: Literal["p300_multi_gen2", "p20_multi_gen2"],
-    tiprack: protocol_api.labware.Labware,
-    mount: Literal["left", "right"],
-) -> protocol_api:
-    return protocol.load_instrument(pipette_type, mount, tip_racks=[tiprack])
-
-
 def run(protocol: protocol_api.ProtocolContext):
     tiprack = get_tiprack(protocol, "opentrons_96_tiprack_300ul", "7")
     right_pipette = protocol.load_instrument(
@@ -51,3 +26,35 @@ def run(protocol: protocol_api.ProtocolContext):
     right_pipette.dispense(200, plate_1.wells_by_name()["A2"])
     response = requests.get(url)
     right_pipette.drop_tip(tiprack.wells_by_name()["A1"])
+
+
+class OpenTronsProtocol:
+    def __init__(self, protocol: protocol_api.ProtocolContext) -> None:
+        self.protocol: protocol_api.ProtocolContext = protocol
+
+    def get_tiprack(
+        self,
+        protocol: protocol_api.ProtocolContext,
+        tiprack_type: Literal[
+            "opentrons_96_tiprack_300ul", "opentrons_96_tiprack_20ul"
+        ],
+        slot: Literal["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"],
+    ) -> protocol_api.labware.Labware:
+        return protocol.load_labware(tiprack_type, slot)
+
+    def load_plate(
+        self,
+        protocol: protocol_api.ProtocolContext,
+        plate_type: Literal["corning_96_wellplate_360ul_flat"],
+        slot: Literal["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"],
+    ) -> protocol_api.labware.Labware:
+        return protocol.load_labware(plate_type, slot)
+
+    def load_pipette(
+        self,
+        protocol: protocol_api.ProtocolContext,
+        pipette_type: Literal["p300_multi_gen2", "p20_multi_gen2"],
+        tiprack: protocol_api.labware.Labware,
+        mount: Literal["left", "right"],
+    ) -> protocol_api:
+        return protocol.load_instrument(pipette_type, mount, tip_racks=[tiprack])
