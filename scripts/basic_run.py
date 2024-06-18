df0472971d285fef3d9ec743fd0b0e109a7f58c9
ikeda042 <ikeda042main@gmail.com>
2024-06-18 18:55:36 +0900

    T

--- scripts/basic_run.py
+++ scripts/basic_run.py
@@ -27,15 +27,15 @@ def run(protocol: protocol_api.ProtocolContext):
     for i in range(40):
         right_pipette.pick_up_tip(tiprack.wells_by_name()["A3"])
         left_pipette.pick_up_tip(tiprack2.wells_by_name()["A1"])
-        for i in range(12):
-            right_pipette.aspirate(200, tray[f"A{i%12+1}"])
-            left_pipette.aspirate(20, tray2[f"A{i%12+1}"])
-            right_pipette.dispense(220, tray2[f"A{i%12+1}"])
-            left_pipette.dispense(25, tray[f"A{i%12+1}"])
-        for i in range(12):
-            right_pipette.aspirate(200, tray2[f"A{i%12+1}"])
-            left_pipette.aspirate(20, tray[f"A{i%12+1}"])
-            right_pipette.dispense(220, tray[f"A{i%12+1}"])
-            left_pipette.dispense(25, tray2[f"A{i%12+1}"])
+        for i in range(4):
+            right_pipette.aspirate(200, tray[f"A{i%12+4}"])
+            left_pipette.aspirate(20, tray2[f"A{i%12+4}"])
+            right_pipette.dispense(220, tray2[f"A{i%12+4}"])
+            left_pipette.dispense(25, tray[f"A{i%12+4}"])
+        for i in range(4):
+            right_pipette.aspirate(200, tray2[f"A{i%12+4}"])
+            left_pipette.aspirate(20, tray[f"A{i%12+4}"])
+            right_pipette.dispense(220, tray[f"A{i%12+4}"])
+            left_pipette.dispense(25, tray2[f"A{i%12+4}"])
         right_pipette.drop_tip(tiprack.wells_by_name()["A1"])
         left_pipette.drop_tip(tiprack2.wells_by_name()["A1"])
