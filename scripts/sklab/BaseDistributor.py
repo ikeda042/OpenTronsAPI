cff2104a325bb59a16217187b4475b4028c0d3b2
ikeda042 <ikeda042main@gmail.com>
2024-07-30 16:58:16 +0900

    Dist

--- scripts/sklab/BaseDistributor.py
+++ scripts/sklab/BaseDistributor.py
@@ -70,10 +70,10 @@ class BaseDistributor:
         )
 
     def distribute(self, aspirate_height_in_mm: float | None = 10.0) -> None:
-        self.right_pipette.pick_up_tip(self.tiprack.wells_by_name()["A1"])
+        self.right_pipette.pick_up_tip(self.tiprack.wells_by_name()["A2"])
         for n in range(1, 13):
             self.right_pipette.aspirate(
-                100, self.reservoir.wells_by_name()["A1"].bottom(aspirate_height_in_mm)
+                100, self.reservoir.wells_by_name()["A7"].bottom(aspirate_height_in_mm)
             )
             self.right_pipette.dispense(100, self.microplate.wells_by_name()[f"A{n}"])
         self.right_pipette.drop_tip(self.tiprack.wells_by_name()["A1"])
