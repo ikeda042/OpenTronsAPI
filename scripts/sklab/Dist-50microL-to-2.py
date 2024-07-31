96084115c29be50b7ccf8c1b82dd0695ecd33d58
ikeda042 <ikeda042main@gmail.com>
2024-07-31 12:45:23 +0900

    Dist50

--- scripts/sklab/Dist-50microL-to-2.py
+++ scripts/sklab/Dist-50microL-to-2.py
@@ -94,7 +94,7 @@ class Dist50to2:
             )
 
     def distribute(self, aspirate_height_in_mm: float | None = 10.0) -> None:
-        self.right_pipette.pick_up_tip(self.tiprack.wells_by_name()["A2"])
+        self.right_pipette.pick_up_tip(self.tiprack.wells_by_name()["A4"])
         for n, j in enumerate(self.dist_amounts):
             for w in range(1, 2):
                 for i in range(3):
