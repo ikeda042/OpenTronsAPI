24a587eb9b3fa4c987b0151f785ac3a0affa93d1
ikeda042 <ikeda042main@gmail.com>
2024-07-31 12:22:41 +0900

    Dist

--- scripts/sklab/Dist-50microL-to-2.py
+++ scripts/sklab/Dist-50microL-to-2.py
@@ -58,8 +58,8 @@ class LabwareLoader:
 
 
 ## Base settings
-plates_to_use: list[int] = [4, 5, 7, 8, 10, 11]
-dist_amounts: list[int] = [100, 100, 100, 100, 100, 100]
+plates_to_use: list[int] = [2]
+dist_amounts: list[int] = [50]
 plates_to_use: list[PlateIndex] = [str(i) for i in plates_to_use]
 
 
